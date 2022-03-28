087105f05f0a80165655506d222d77029f2b2a4f
cheedli <chedhly.ghorbel@esprit.tn>
2022-03-27 23:44:15 -0700

    t

--- ORIGINAL GAME/chedli/ennemi.c
+++ ORIGINAL GAME/chedli/ennemi.c
@@ -1,30 +1,101 @@
-#include "ennemi.h"
+ #include "ennemi.h"
 
 
-void initEnnemi(Ennemi *e[] )
+void initEnnemi(Ennemi g[],Ennemi a[],Ennemi l[],Ennemi c[] )
 {
 int i;
-e[0]->image =IMG_Load("graphic/menu/.png");
-e[1]->image =IMG_Load("graphic/menu/.png");
-e[2]->image =IMG_Load("graphic/menu/.png");
-e[3]->image =IMG_Load("graphic/menu/.png");
+
+//g is el ghoul
+
+g[0]->image =IMG_Load("graphic/menu/.png");//standing
+
+g[0]->pos2.h=g[0].img->h;
+g[0]->pos2.w=g[0].img->w/3;
+
+g[1]->image =IMG_Load("graphic/menu/.png");//dead
+
+g[1]->pos2.h=g[1].img->h;
+g[1]->pos2.w=g[1].img->w/3;
+
+//a is azouzet estoot
+
+a[0]->image =IMG_Load("graphic/menu/.png");// going right and left
+
+a[0]->pos2.h=a[0].img->h;
+a[0]->pos2.w=a[0].img->w/3;
+
+a[1]->image =IMG_Load("graphic/menu/.png");//attack
+
+a[1]->pos2.h=a[1].img->h;
+a[1]->pos2.w=a[1].img->w/3;
+
+a[2]->image =IMG_Load("graphic/menu/.png");//dead
+
+a[2]->pos2.h=a[2].img->h;
+a[2]->pos2.w=a[2].img->w/3;
+
+
+//l is boulaaba
+
+l[0]->image =IMG_Load("graphic/menu/.png");//runing to the player
+
+l[0]->pos2.h=l[0].img->h;
+l[0]->pos2.w=l[0].img->w/3;
+
+l[1]->image =IMG_Load("graphic/menu/.png");//attacking
+
+l[1]->pos2.h=l[1].img->h;
+l[1]->pos2.w=l[1].img->w/3;
+
+//c is bouchkara
+
+c[0]->image =IMG_Load("graphic/menu/.png");//coming to the player
+
+c[0]->pos2.h=c[0].img->h;
+c[0]->pos2.w=c[0].img->w/3;
+
+c[1]->image =IMG_Load("graphic/menu/.png");//attacking
+
+c[1]->pos2.h=c[1].img->h;
+c[1]->pos2.w=c[1].img->w/3;
+
+c[2]->image =IMG_Load("graphic/menu/.png");//dead
+
+c[2]->pos2.h=c[2].img->h;
+c[2]->pos2.w=c[2].img->w/3;
 
 for(i=0;i<3;i++)
-{
-e[i]->pos.x=0;
-e[i]->pos.y=0;
-e[i]->pos.h=e[i]->img->h;
-e[i]->posw.w=e[i]->img->w;
-}
+
+	{
+		g[i]->pos.x=0;
+		g[i]->pos.y=0;
+
+		a[i]->pos.x=0;
+		a[i]->pos.y=0;
+
+		c[i]->pos.x=0;
+		c[i]->pos.y=0;
+
+		l[i]->pos.x=0;
+		l[i]->pos.y=0;	
+	}
 
 }
 
 void afficherEnnemi(Ennemi e, SDL_Surface * screen,SDL_Rect pos)
-{
+{		
 SDL_Blitsurface(e.img,&e.pos2,screen,pos);
 }
-
 void animerEnnemi( Ennemi * e)
+{
+	
+			if(e->pos2.x == 1200)
+				e->pos2.x = 0;
+			else
+				e->pos2.x = e->pos2.x + e->pos2.w;
+			
+	
+}			
 
 int collisionBox(SDL_Rect a, SDL_Rect b)
 {
--- ORIGINAL GAME/chedli/ennemi.h
+++ ORIGINAL GAME/chedli/ennemi.h
@@ -10,58 +10,19 @@
 
 
 
-struct ennemi{
-	int orientation;
-	int esprit;
+struct ennemi
+	{
+	SDL_Surface *img;
+	SDL_Rect pos,pos2;
+	int etat;
 	
-	SDL_Surface *image;
-	
-	SDL_Rect position;
-	SDL_Rect positionRelative;
-	
-	int tempsActuel;
-	
-	SDL_Rect clipsRight[20];
-	SDL_Rect clipsLeft[20];
-	
-	int speed;
-	int frame;
-	int status;
-};
+	};
 
 void afficher_enemie (enemie *f,SDL_Surface *screen,SDL_Rect pos);
 void animerEnnemi( enemie * e);
 void deplacer( enemie * e);
 int collisionBB( SDL_Rect posp, SDL_Rect pose);
-
+void deplacerAI( enemie * e);
 
 
 #endif
-
- 
-
-
-
-typedef struct 
-	{
-
-	SDL_Surface *image_enemie;
-	SDL_Rect rect;
-
-	SDL_Rect rects[5][14];
-	int frame;
-	int direction;	
-  	int range;
-	int dead;
-	}enemie;
-
-
-void LAMTI(enemie *E,int range);
-void LAMTI2(SDL_Rect* clip[5][14]);
-void LAMTI1(enemie*f);
-
-void update_ennemi(enemie *E,SDL_Surface *screen,SDL_Rect r,int p);
-
-
-
-#endif // 2NDENTITE_H_INCLUDED
