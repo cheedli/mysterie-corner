8dc7cefcaf5ae5390368f022f3351dc2f7d08760
cheedli <chedhly.ghorbel@esprit.tn>
2022-04-06 16:02:25 -0700

    e

--- ORIGINAL GAME/maze/enigme.c
+++ ORIGINAL GAME/maze/enigme.c
@@ -84,24 +84,174 @@ void afficherEnigme(enigme e, SDL_Surface * screen)
 {
 SDL_BlitSurface (e.img,NULL,screen,&e.pos);
 }
-
-void initexte(texte *A)
+void animer (enigme *e)
 {
-    A->position.x=0;
-    A->position.y=0; 
+TTF_Init();
+//      ***************************INITIALISATION***************************
+	SDL_Surface *screen ;
+	SDL_Event event;
+	enigme es[6] , timer[10],back;
+	int  a=0,b=2,c=4;
+	int cont = 1;
+	int o,t=0,i=0;
+	  char cc[20];
+		text tt;
+    	time_t start,end;
+    start=clock();
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+ 	{
+		printf("Unable to initialisation SDL : %s\n",SDL_GetError());
+	}
+	SDL_WM_SetCaption("ENIGME",NULL);
+	screen = SDL_SetVideoMode(1300,780,32,SDL_HWSURFACE | SDL_DOUBLEBUF);
+	if (screen == NULL)
+	{
+		printf("error : %s\n",SDL_GetError());
+	}
+	init_enigme(es,&back,timer );
+	
+	initexte(&tt);
+
+ 
+
+	
+//      ***************************BOUCLE***************************
+	
+	while (cont)
+	{	 
+
+  sprintf(cc, ":0%d", t);
+
+
+
+      	afficherEnigme(back, screen);
+  	afficherEnigme(timer[t], screen);
+  	afficherEnigme(es[a], screen);
+    	afficherEnigme(es[b], screen);
+      	afficherEnigme(es[c], screen);
+      	displayText(tt,screen,cc);
+      SDL_PollEvent(&event);
+      
+      end=clock();
+t=(end-start)/CLOCKS_PER_SEC;
+
+if(o!=t)
+    printf("%d\n",t);
+    if(t==10)
+    {
+        cont=0;
+    }
+    o=t;
+    	
+		if ((event.type == SDL_QUIT)||(event.key.keysym.sym == SDLK_ESCAPE))
+				cont = 0;	
+		SDL_Flip(screen);
+			a=0;
+			b=2;
+			c=4;
+
+		
+		switch(event.type)
+        {
+        
+	case SDL_KEYDOWN:
+					SDL_Delay(120);	
+
+	switch(event.key.keysym.sym)
+	{
+	
+	
+	             case SDLK_a:
+	             if(a!=0)
+			a=0;
+		else 
+			{
+			a=1;
+			
+			
+			
+			}
+	              break;
+	              case SDLK_q:
+			if(a!=0)
+			a=0;
+		else 
+			{
+			a=1;
+			
+			
+			}
+	              break;
+	               case SDLK_b:
+	               if(b!=2)
+			b=2;
+		else 
+
+			{
+
+			b=3;
+
+			
+			}
+	              break;
+	               case SDLK_c:
+	               if(c!=4)
+			c=4;
+		else 
+
+	               
+{
+			
+			c=5;
+			
+			}
+	              break;
+	              		
+	              
+	            
+	
+	}	              break;
+	case SDL_MOUSEMOTION: 
+            	
+		
+			
+            if (event.motion.x>170&& event.motion.y>350 && event.motion.x<380&& event.motion.y<600)
+     
+            a=1;
+            
+            if (event.motion.x>520&& event.motion.y>350 && event.motion.x<710&& event.motion.y<600)
+     
+            b=3;
+            
+            if (event.motion.x>880&& event.motion.y>350 && event.motion.x<1090&& event.motion.y<600)
+     
+            c=5;
+            
+	break;
+	
+            	
+	}
+	
+
+	}
+
+}
+
+void initexte(text *A)
+{
+    A->position.x=1240;
+    A->position.y=15; 
 // couleur noir
     A->textColor.r=255;
     A->textColor.g=255;
     A->textColor.b=255;
 
-    A->font = TTF_OpenFont( "font.ttf", 50 );
+    A->font = TTF_OpenFont( "police.ttf", 20 );
 }
 
 
-void displayText(texte t,SDL_Surface *screen,char cc[20])
+void displayText(text t,SDL_Surface *screen,char cc[30])
 {
     t.surfaceTexte = TTF_RenderText_Solid(t.font, cc, t.textColor );
     SDL_BlitSurface(t.surfaceTexte, NULL, screen,&t.position);
 }
-
-
