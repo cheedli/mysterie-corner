ec1ccbf15d699b03945cace2d1b046ce62ea415c
KurMa69 <99597354+KurMa69@users.noreply.github.com>
2022-04-10 17:54:43 +0100

    Add files via upload

---
+++ test/test.h
@@ -0,0 +1,57 @@
+ #ifndef test_H_INCLUDED
+#define test_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <stdbool.h>
+#include <math.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+
+
+typedef struct perso
+{
+double vitesse,   acceleration;
+SDL_Surface *personnage;
+SDL_Rect rect;
+SDL_Rect pos_sprite;
+
+float vitesse_V;
+int direction ; 
+/*
+les directions:
+0:stable droite
+1:droite
+2:gauche
+3: jump
+
+*/
+
+}perso;
+
+typedef struct Input
+{
+ int left,right,jump;
+
+} Input;
+void initialiser_walk (perso *p);
+void initialiser_wl (perso *p);
+void initialiser_jump (perso *p);
+void initialiser_perso (perso *p);
+void afficher_perso (perso p,SDL_Surface *screen);
+
+void animation (perso *p,Input i);
+void movePerso(perso *p,Uint32 dt,Input i);
+
+// acceleration
+
+
+void initialiser_input (Input *I);
+
+void saut(perso *p);
+
+
+ #endif
+
