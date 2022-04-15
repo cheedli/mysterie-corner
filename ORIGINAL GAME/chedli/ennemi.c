363c3fd0f0faf8a67acf0d44dc83cf6531333a1f
cheedli <chedhly.ghorbel@esprit.tn>
2022-04-15 11:38:01 -0700

    r

--- ORIGINAL GAME/chedli/ennemi.c
+++ ORIGINAL GAME/chedli/ennemi.c
@@ -5,81 +5,86 @@ void initEnnemi(Ennemi *e)
 
 {
     int i;
-    /*
+    
     //g is el ghoul
+/*
+    e->en.g[0] =IMG_Load("graphic/menu/.png");//standing
+    e->en.g[1] =IMG_Load("graphic/menu/.png");//dead
 
-    g[0].img =IMG_Load("graphic/menu/.png");//standing
+    e->en.posg[0].x=0;
+    e->en.posg[0].y=0;
+    e->en.posg[0].h=e->en.g[0]->h;
+    e->en.posg[0].w=e->en.g[0]->w/3;
+    
+    e->en.posg[1].x=0;
+    e->en.posg[1].y=0;
+    e->en.posg[1].h=e->en.g[1]->h;
+    e->en.posg[1].w=e->en.g[1]->w/3;
+*/
 
-    g[0].pos2.h=g[0].img->h;
-    g[0].pos2.w=g[0].img->w/3;
 
-    g[1].img =IMG_Load("graphic/menu/.png");//dead
 
-    g[1].pos2.h=g[1].img->h;
-    g[1].pos2.w=g[1].img->w/3;
+    
 
     //a is azouzet estoot
-    */
+    
 
-   e->en.a[0]=IMG_Load("as.png");    // going right and left
-   e->en.a[1] =IMG_Load("ashitt.png");//attack
-   e->en.a[2] =IMG_Load("asdead.png");//dead
+    e->en.a[0]=IMG_Load("as.png");    // going right and left
+    e->en.a[1] =IMG_Load("ashitt.png");//attack
+    e->en.a[2] =IMG_Load("asdead.png");//dead
 
 
     e->en.posa[0].x=0;
     e->en.posa[0].y=0;
     e->en.posa[0].h=e->en.a[0]->h;
     e->en.posa[0].w=e->en.a[0]->w/10;
-    
+
     e->en.posa[1].x=0;
     e->en.posa[1].y=0;
     e->en.posa[1].h=e->en.a[1]->h;
     e->en.posa[1].w=e->en.a[1]->w/5;
-    
+
     e->en.posa[2].x=0;
     e->en.posa[2].y=0;
     e->en.posa[2].h=e->en.a[2]->h;
     e->en.posa[2].w=e->en.a[2]->w/5;
-    
-
-									
-    
-/*
-    a[2].img =IMG_Load("graphic/menu/.png");//dead
-
-    a[2].pos2.h=a[2].img->h;
-    a[2].pos2.w=a[2].img->w/3;
 
 
-    //l is boulaaba
 
-    l[0].img =IMG_Load("graphic/menu/.png");//runing to the player
 
-    l[0].pos2.h=l[0].img->h;
-    l[0].pos2.w=l[0].img->w/3;
+    /*
 
-    l[1].img =IMG_Load("graphic/menu/.png");//attacking
 
-    l[1].pos2.h=l[1].img->h;
-    l[1].pos2.w=l[1].img->w/3;
 
-    //c is bouchkara
+        //l is boulaaba
 
-    c[0].img =IMG_Load("graphic/menu/.png");//coming to the player
+        l[0].img =IMG_Load("graphic/menu/.png");//runing to the player
 
-    c[0].pos2.h=c[0].img->h;
-    c[0].pos2.w=c[0].img->w/3;
+        l[0].pos2.h=l[0].img->h;
+        l[0].pos2.w=l[0].img->w/3;
 
-    c[1].img =IMG_Load("graphic/menu/.png");//attacking
+        l[1].img =IMG_Load("graphic/menu/.png");//attacking
 
-    c[1].pos2.h=c[1].img->h;
-    c[1].pos2.w=c[1].img->w/3;
+        l[1].pos2.h=l[1].img->h;
+        l[1].pos2.w=l[1].img->w/3;
 
-    c[2].img =IMG_Load("graphic/menu/.png");//dead
+        //c is bouchkara
 
-    c[2].pos2.h=c[2].img->h;
-    c[2].pos2.w=c[2].img->w/3;
-    */
+        c[0].img =IMG_Load("graphic/menu/.png");//coming to the player
+
+        c[0].pos2.h=c[0].img->h;
+        c[0].pos2.w=c[0].img->w/3;
+
+        c[1].img =IMG_Load("graphic/menu/.png");//attacking
+
+        c[1].pos2.h=c[1].img->h;
+        c[1].pos2.w=c[1].img->w/3;
+
+        c[2].img =IMG_Load("graphic/menu/.png");//dead
+
+        c[2].pos2.h=c[2].img->h;
+        c[2].pos2.w=c[2].img->w/3;
+        */
     for(i=0; i<3; i++)
 
     {
@@ -89,8 +94,8 @@ void initEnnemi(Ennemi *e)
         e->pos[1].x=500;
         e->pos[1].y=320;
         e->pos[1].h=e->en.a[0]->h;
-    	e->pos[1].w=e->en.a[0]->w/10;
-        
+        e->pos[1].w=e->en.a[0]->w/10;
+
 
         e->pos[2].x=0;
         e->pos[2].y=0;
@@ -118,7 +123,7 @@ void afficherEnnemi(Ennemi e, SDL_Surface * screen)
     case 3:
         SDL_BlitSurface(e.en.b[e.etat],&e.en.posb[e.etat],screen,&e.pos[e.level]);
         break;
-        
+
     }
 
 }
@@ -127,90 +132,82 @@ void animerEnnemi( Ennemi * e)
 
 {
 
-switch(e->level)
-{
-case 1:
-switch(e->etat)
-{
-case 0:
-    if(e->en.posa[e->etat].x <1800)
-        e->en.posa[e->etat].x +=  e->en.posa[e->etat].w;
-    else
+    switch(e->level)
     {
-        e->en.posa[e->etat].x =0;
-        e->pos[e->level].x =500;
-    }    SDL_Delay(230);
+    case 1:
+        switch(e->etat)
+        {
+        case 0:
+            if(e->en.posa[e->etat].x <1800)
+                e->en.posa[e->etat].x +=  e->en.posa[e->etat].w;
+            else
+            {
+                e->en.posa[e->etat].x =0;
+                e->pos[e->level].x =500;
+            }
+            SDL_Delay(230);
 
-break;
-case 1:
-    if(e->en.posa[e->etat].x <800)
-        e->en.posa[e->etat].x +=  e->en.posa[e->etat].w;
-    else
-    {
-        e->en.posa[e->etat].x =0;
+            break;
+        case 1:
+            if(e->en.posa[e->etat].x <800)
+                e->en.posa[e->etat].x +=  e->en.posa[e->etat].w;
+            else
+            {
+                e->en.posa[e->etat].x =0;
 
-    }    SDL_Delay(120);
+            }
+            SDL_Delay(120);
 
-    break;
-    case 2:
-    if(e->en.posa[e->etat].x <1000)
-    {
-        e->en.posa[e->etat].x +=  e->en.posa[e->etat].w;
-      }
-        SDL_Delay(400);
+            break;
+        case 2:
+            if(e->en.posa[e->etat].x <1000)
+            {
+                e->en.posa[e->etat].x +=  e->en.posa[e->etat].w;
+            }
+            SDL_Delay(400);
 
-    break;
+            break;
 
-}
-break;
-}
+        }
+        break;
+    }
 }
 
 void deplacer(Ennemi *e)
 {
-switch(e->level)
-{
-case 1:
-switch(e->etat)
-{
-case 0:
-
-    if(e->en.posa[e->etat].x >=1000)
+    switch(e->level)
     {
-        e->pos[e->level].x -= 15;
-    }
-    else if(e->pos[e->level].x <1000)
+    case 1:
+        switch(e->etat)
+        {
+        case 0:
 
-        e->pos[e->level].x+=15;
-        break ; 
-  case 1:
+            if(e->en.posa[e->etat].x >=1000)
+            {
+                e->pos[e->level].x -= 15;
+            }
+            else if(e->pos[e->level].x <1000)
 
-    if((e->pos[e->level].x <=1000)&&(e->pos[e->level].x >=400))
-    {
-        e->pos[e->level].x -= 15;
-    }
-            break ; 
-    
-        
+                e->pos[e->level].x+=15;
+            break ;
+        case 1:
+
+            if((e->pos[e->level].x <=1000)&&(e->pos[e->level].x >=400))
+            {
+                e->pos[e->level].x -= 15;
+            }
+            break ;
         }
+    }
 }
-}
-
-
-
-
-
-
 
 int collisionBox(SDL_Rect a, SDL_Rect b)
-{ 
+{
     if(a.x+a.h<b.x || a.x>b.x+b.w ||a .y+a.h<b.y || a.y>b.y+b.h)
         return 0;
-        else 
+    else
         return 1;
-
-    
-
-
 }
+
+
 
--- ORIGINAL GAME/chedli/mainennemi.c
+++ ORIGINAL GAME/chedli/mainennemi.c
@@ -3,80 +3,86 @@
 void main()
 {
 //      ***************************INITIALISATION***************************
-	SDL_Surface *screen = NULL;
-	SDL_Event event;
-	Ennemi e;
-	image back,colision;
-	SDL_Rect posmousse;
-	int cont = 1,i=0;
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
- 	{
-		printf("Unable to initialisation SDL : %s\n",SDL_GetError());
-	}
-	SDL_WM_SetCaption("ENNEMI",NULL);
-	screen = SDL_SetVideoMode(1380,700,32,SDL_HWSURFACE|SDL_SWSURFACE|SDL_RESIZABLE|SDL_DOUBLEBUF);
-	if (screen == NULL)
-	{
-		printf("error : %s\n",SDL_GetError());
-	}
-	int x,y;
-	Uint32 buttons;
+    SDL_Surface *screen = NULL;
+    SDL_Event event;
+    Ennemi e;
+    image back,colision;
+    SDL_Rect posmousse;
+    int cont = 1,i=0;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        printf("Unable to initialisation SDL : %s\n",SDL_GetError());
+    }
+    SDL_WM_SetCaption("ENNEMI",NULL);
+    screen = SDL_SetVideoMode(1380,700,32,SDL_HWSURFACE|SDL_SWSURFACE|SDL_RESIZABLE|SDL_DOUBLEBUF);
+    if (screen == NULL)
+    {
+        printf("error : %s\n",SDL_GetError());
+    }
+    
 
-colision.img = IMG_Load ("w");
-	colision.pos.x = 0;
-	colision.pos.y = 350;
-	colision.pos.h=colision.pos.h;
-   colision.pos.w=colision.pos.w;
+    colision.img = IMG_Load ("w");
+    colision.pos.x = 0;
+    colision.pos.y = 350;
+    colision.pos.h=colision.pos.h;
+    colision.pos.w=colision.pos.w;
 
-	back.img = IMG_Load ("file.jpg");
-	back.pos.x = 0;
-	back.pos.y = 0;
-	initEnnemi(&e);
+    back.img = IMG_Load ("file.jpg");
+    back.pos.x = 0;
+    back.pos.y = 0;
+    initEnnemi(&e);
 //      ***************************BOUCLE***************************
-	e.level=1;
-	e.etat=0;
-	while (cont)
-	{
+    e.level=1;
+    e.etat=0;
+    while (cont)
+    {
 
-	switch(e.etat)
-	{
-	case 0:
-	colision.pos.x += 5;
-		if (collisionBox( e.pos[e.level],colision.pos)==1)
-		
-		e.etat=1;
-	
-	
-	if (event.type == SDL_MOUSEBUTTONDOWN)
-	{
-	 if (event.button.button==SDL_BUTTON_LEFT)
-            {	e.etat=1;}}
-		break;
-		
+        switch(e.etat)
+        {
+        case 0:
+            colision.pos.x += 5;
+            if (collisionBox( e.pos[e.level],colision.pos)==1)
 
-		
-	case 1:
-		
-	if (event.type == SDL_MOUSEBUTTONDOWN)
-	{
-	 if (event.button.button==SDL_BUTTON_LEFT)
-            {	e.etat=2;}}
-		break;
-		}
+                e.etat=1;
 
-	SDL_PollEvent(&event);
-	SDL_BlitSurface(back.img,NULL,screen,&back.pos);	
-	SDL_BlitSurface(colision.img,NULL,screen,&colision.pos);	
-	afficherEnnemi(e,screen);	
-	animerEnnemi(&e);
-	deplacer(&e);
-	SDL_Flip(screen);
 
-	if ((event.type == SDL_QUIT)||(event.key.keysym.sym == SDLK_ESCAPE))
-				cont = 0;		
-		
-	
-	
-	}
-	SDL_Quit();
+            if (event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                if (event.button.button==SDL_BUTTON_LEFT)
+                {
+                    e.etat=1;
+                }
+            }
+            break;
+
+
+
+        case 1:
+
+            if (event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                if (event.button.button==SDL_BUTTON_LEFT)
+                {
+                    e.etat=2;
+                }
+            }
+            break;
+        }
+
+        SDL_PollEvent(&event);
+        SDL_BlitSurface(back.img,NULL,screen,&back.pos);
+        SDL_BlitSurface(colision.img,NULL,screen,&colision.pos);
+        afficherEnnemi(e,screen);
+        animerEnnemi(&e);
+        deplacer(&e);
+        SDL_Flip(screen);
+
+        if ((event.type == SDL_QUIT)||(event.key.keysym.sym == SDLK_ESCAPE))
+            cont = 0;
+
+
+
+    }
+    SDL_Quit();
 }
+
